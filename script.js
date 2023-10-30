7864e3167a5adaad6782e934b618a9bb285c633b
Brian Joe Antoni <brianjoeantoni@gmail.com>
2023-10-30 14:33:28 +0700

    window.scrolly udate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sidebar = document.querySelector(".side-bar");
 const sidebarOverlay = document.querySelector(".side-bar-overlay");
 window.addEventListener("scroll", () => {
-  if (window.scrollY != 0) {
+  if (window.scrollY > 50) {
     // console.log("down");
     sidebar.classList.add("side-bar--hidden");
     sidebarOverlay.classList.add("side-bar--hidden");
